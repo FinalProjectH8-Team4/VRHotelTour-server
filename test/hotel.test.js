5d0d587c44d05ccf922c6c3bdb8ab2f9acdb136b
hutamy <hutamytriesthi@gmail.com>
2020-12-12 13:57:40 +0700

    Add testing for post, put, and delete

--- test/hotel.test.js
+++ test/hotel.test.js
@@ -68,4 +68,86 @@ describe('TEST POST Hotel /', () => {
         }))
         done()
     })
+
+    // it('Test post hotel failed, no payload', async (done) => {
+    //     const res = await request.post('/').send()
+    //     expect(res.status).toBe(500)
+    //     expect(res.body).toEqual(expect.objectContaining({
+    //         msg: expect.any(String)
+    //     }))
+    //     done()
+    // })
+})
+
+describe('TEST PUT Hotel /:id', () => {
+    it('Test PUT hotel sucess', async (done) => {
+        let id = '5fd304867e7b384e1eac9345'
+        const payload = testPayload
+        const res = await request.put(`/${id}`).send(payload)
+        expect(res.status).toBe(200)
+        expect(res.body).toEqual(expect.objectContaining({
+            _id: expect.any(String),
+            name: expect.any(String),
+            room_type: expect.any(Array),
+            facilities: expect.any(Array)
+        }))
+        done()
+    })
+
+    it('Test PUT hotel failed, invalid ID', async (done) => {
+        let id = '123456abcdef'
+        const payload = testPayload
+        const res = await request.put(`/${id}`).send(payload)
+        expect(res.status).toBe(404)
+        expect(res.body).toEqual(expect.objectContaining({
+            msg: expect.any(String)
+        }))
+        done()
+    })
+
+    it('Test PUT hotel failed, no ID', async (done) => {
+        let id = null
+        const payload = testPayload
+        const res = await request.put(`/${id}`).send(payload)
+        expect(res.status).toBe(500)
+        expect(res.body).toEqual(expect.objectContaining({
+            msg: expect.any(String)
+        }))
+        done()
+    })
+})
+
+describe('Test DELETE hotel /:id', () => {
+    it('Test DELETE hotel success', async (done) => {
+        let id = '5fd304867e7b384e1eac9345'
+        const res = await request.delete(`/${id}`)
+        expect(res.status).toBe(200)
+        expect(res.body).toEqual(expect.objectContaining({
+            _id: expect.any(String),
+            name: expect.any(String),
+            room_type: expect.any(Array),
+            facilities: expect.any(Array)
+        }))
+        done()
+    })
+
+    it('Test DELETE hotel failed, invalid ID', async (done) => {
+        let id = '123456abcdef'
+        const res = await request.delete(`/${id}`)
+        expect(res.status).toBe(404)
+        expect(res.body).toEqual(expect.objectContaining({
+            msg: expect.any(String)
+        }))
+        done()
+    })
+
+    it('Test DELETE hotel, no ID', async (done) => {
+        let id = null
+        const res = await request.delete(`/${id}`)
+        expect(res.status).toBe(500)
+        expect(res.body).toEqual(expect.objectContaining({
+            msg: expect.any(String)
+        }))
+        done()
+    })
 })
